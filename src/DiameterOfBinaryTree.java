b948444dd89c6138e0d802b0ce6171f2f25a095f
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-08-18 22:43:43 -0700

    coded the main method and the recursive method for results

--- src/DiameterOfBinaryTree.java
+++ src/DiameterOfBinaryTree.java
@@ -13,7 +13,27 @@ class TreeNode{
 
 
 public class DiameterOfBinaryTree {
+    static int ans =0;
+    static int rec(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+        int left = rec(root.left);
+        int right = rec(root.right);
+        ans = Math.max(ans, left + right +1);
+        return Math.max(left,right)+1;
+    }
 
+
+    public static int diameterOfBinaryTree(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+        ans=0;
+        rec(root);
+        return ans;
+
+    }
 
     public static void main(String[] args) {
 
