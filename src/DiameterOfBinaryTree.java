6cf6349dc23d8ac1f7cf8cb04bd1838f84cb525e
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-08-18 22:38:47 -0700

    code the TreeNode clas

--- src/DiameterOfBinaryTree.java
+++ src/DiameterOfBinaryTree.java
@@ -1,5 +1,21 @@
+import com.sun.source.tree.Tree;
+
+class TreeNode{
+    int data;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int data, TreeNode left, TreeNode right){
+        this.data =data;
+        this.left = left;
+        this.right =right;
+    }
+}
+
+
 public class DiameterOfBinaryTree {
+
+
     public static void main(String[] args) {
-        
+
     }
 }
