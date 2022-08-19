706e8a595ad7b4bf7a028686d799ab99b978ed21
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-08-18 22:47:22 -0700

    added comments with some confusions

--- src/DiameterOfBinaryTree.java
+++ src/DiameterOfBinaryTree.java
@@ -1,9 +1,10 @@
-import com.sun.source.tree.Tree;
 
+//TreeNode class
 class TreeNode{
     int data;
     TreeNode left;
     TreeNode right;
+
     TreeNode(int data, TreeNode left, TreeNode right){
         this.data =data;
         this.left = left;
@@ -13,15 +14,18 @@ class TreeNode{
 
 
 public class DiameterOfBinaryTree {
+    //static int to use it everywhere
     static int ans =0;
-    static int rec(TreeNode root){
-        if(root == null){
+    static int rec(TreeNode root) {
+        if (root == null) {
             return 0;
         }
         int left = rec(root.left);
         int right = rec(root.right);
-        ans = Math.max(ans, left + right +1);
-        return Math.max(left,right)+1;
+        ans = Math.max(ans, left + right + 1);  //doing +1 here to include the root node as well
+        //idk why but if i only need to return ans at the end why do i need to return this statement
+        return Math.max(left, right) + 1;
+
     }
 
 
@@ -31,7 +35,7 @@ public class DiameterOfBinaryTree {
         }
         ans=0;
         rec(root);
-        return ans;
+        return ans - 1;   // doing -1 here as I just needed the number of edges
 
     }
 
